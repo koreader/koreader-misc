99c11d338e505e0c395d8257e048584a188c11b2
Huang Xin <chrox.huang@gmail.com>
2013-03-25 12:21:47 -0400

    add nightlybuild script for koreader

---
+++ koreader-nightlybuild.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SUMMARY="koreader-nightly-`date +%Y%m%d`"
+PROJECT="koreader-package"
+USER="******@gmail.com"
+PASSWORD="************"
+
+echo "clone koreader to dev directory"
+cd ~/dev
+rm -rf koreader
+git clone git://github.com/koreader/koreader.git
+cd koreader
+echo "compile and build customupdate"
+PATH="/opt/arm-2012.03/bin:$PATH"
+make fetchthirdparty
+make customupdate
+korzip=`find koreader*.zip |sort -r |head -1`
+# upload to Google code
+~/bin/googlecode_upload.py -s $SUMMARY -p $PROJECT -u $USER -w $PASSWORD $korzip
