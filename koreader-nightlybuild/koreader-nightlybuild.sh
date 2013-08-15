0e9e878e2033f502e1263a6fdfabaad102abf306
Huang Xin <chrox.huang@gmail.com>
2013-08-15 23:11:21 +0800

    Update koreader-nightlybuild.sh

--- koreader-nightlybuild/koreader-nightlybuild.sh
+++ koreader-nightlybuild/koreader-nightlybuild.sh
@@ -10,8 +10,13 @@ cd koreader
 echo "compile and build customupdate"
 PATH="/opt/arm-2012.03/bin:$PATH"
 USE_FIXED_POINT=1
+USE_NO_CCACHE=1
 make fetchthirdparty
+make
 make customupdate
-korzip=`find koreader*.zip |sort -r |head -1`
+make koboupdate
+kindlezip=`find koreader-kindle*.zip |sort -r |head -1`
+kobozip=`find koreader-kobo*.zip |sort -r |head -1`
 # upload to Google code
-~/bin/googlecode_upload.py -s $SUMMARY -p $PROJECT -u $USER -w $PASSWORD $korzip
+~/bin/googlecode_upload.py -s $SUMMARY -p $PROJECT -u $USER -w $PASSWORD $kobozip
+~/bin/googlecode_upload.py -s $SUMMARY -p $PROJECT -u $USER -w $PASSWORD $kindlezip
