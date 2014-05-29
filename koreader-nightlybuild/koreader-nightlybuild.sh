c26add571c0077f15babe9f1c5c0f02070e84a6a
Huang Xin <chrox.huang@gmail.com>
2014-05-29 21:39:29 +0800

    update to latest nightly build script

--- koreader-nightlybuild/koreader-nightlybuild.sh
+++ koreader-nightlybuild/koreader-nightlybuild.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-source ./koreader-nightlybuild.conf
+koreader_repo="git://github.com/koreader/koreader.git"
+
+export ANDROID_SDK_ROOT=/home/matrix/dev/android-sdk-linux
+export PATH=$ANDROID_SDK_ROOT/tools:$PATH
+export NDK=/home/matrix/dev/android-ndk-r9d
+
+export USE_NO_CCACHE=1
 
 echo "clone koreader to dev directory"
 cd ~/dev
-rm -rf koreader
-git clone git://github.com/koreader/koreader.git
-cd koreader
-echo "compile and build customupdate"
-PATH="/opt/arm-2012.03/bin:$PATH"
-USE_FIXED_POINT=1
-USE_NO_CCACHE=1
-make fetchthirdparty
-make customupdate
+git clone $koreader_repo && cd koreader || cd koreader && git fetch origin
+git reset --hard origin/master
+
+echo "compile and build customupdate for kindle"
 make clean
-make TARGET_DEVICE=KOBO koboupdate
-kindlezip=`find koreader-kindle*.zip |sort -r |head -1`
-kobozip=`find koreader-kobo*.zip |sort -r |head -1`
-# upload to Google code
-~/bin/googlecode_upload.py -s $SUMMARY -p $PROJECT -u $USER -w $PASSWORD $kobozip
-~/bin/googlecode_upload.py -s $SUMMARY -p $PROJECT -u $USER -w $PASSWORD $kindlezip
+make TARGET=kindle fetchthirdparty kindleupdate 
+
+echo "compile and build koboupdate"
+make clean
+make TARGET=kobo fetchthirdparty koboupdate 
+
+echo "compile and build androidupdate"
+make clean
+make TARGET=android fetchthirdparty androidupdate
