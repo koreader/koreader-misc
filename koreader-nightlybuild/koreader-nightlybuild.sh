17e209d3517d43afdc78ff8d6d45c183f5ce6af4
Huang Xin <chrox.huang@gmail.com>
2014-06-04 22:07:49 +0800

    Update koreader-nightlybuild.sh

--- koreader-nightlybuild/koreader-nightlybuild.sh
+++ koreader-nightlybuild/koreader-nightlybuild.sh
@@ -3,24 +3,24 @@
 koreader_repo="git://github.com/koreader/koreader.git"
 
 export ANDROID_SDK_ROOT=/home/matrix/dev/android-sdk-linux
-export PATH=$ANDROID_SDK_ROOT/tools:$PATH
 export NDK=/home/matrix/dev/android-ndk-r9d
+export PATH=$ANDROID_SDK_ROOT/tools:$NDK:$PATH
 
-export USE_NO_CCACHE=1
+#export USE_NO_CCACHE=1
 
 echo "clone koreader to dev directory"
 cd ~/dev
 git clone $koreader_repo && cd koreader || cd koreader && git fetch origin
 git reset --hard origin/master
 
-echo "compile and build customupdate for kindle"
-make clean
-make TARGET=kindle fetchthirdparty kindleupdate 
+echo "fetching thirdparty sources"
+make fetchthirdparty
+
+echo "compile and build kindleupdate"
+make TARGET=kindle clean kindleupdate 
 
 echo "compile and build koboupdate"
-make clean
-make TARGET=kobo fetchthirdparty koboupdate 
+make TARGET=kobo clean koboupdate 
 
 echo "compile and build androidupdate"
-make clean
-make TARGET=android fetchthirdparty androidupdate
+make TARGET=android androidupdate
