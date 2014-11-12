faf33880e0b8ce680e7062be14961021e88ebfc4
Huang Xin <chrox.huang@gmail.com>
2014-11-12 18:18:51 +0800

    Update koreader-nightlybuild.sh

--- koreader-nightlybuild/koreader-nightlybuild.sh
+++ koreader-nightlybuild/koreader-nightlybuild.sh
@@ -12,15 +12,19 @@ echo "clone koreader to dev directory"
 cd ~/dev
 git clone $koreader_repo && cd koreader || cd koreader && git fetch origin
 git reset --hard origin/master
-
-echo "fetching thirdparty sources"
 make fetchthirdparty
 
+echo "update resource file in Transifex"
+make pot 
+
+echo "grab the latest translations"
+make po
+
 echo "compile and build kindleupdate"
-make TARGET=kindle clean kindleupdate 
+make TARGET=kindle clean fetchthirdparty kindleupdate 
 
 echo "compile and build koboupdate"
-make TARGET=kobo clean koboupdate 
+make TARGET=kobo clean fetchthirdparty koboupdate
 
 echo "compile and build androidupdate"
-make TARGET=android androidupdate
+make TARGET=android clean fetchthirdparty androidupdate
